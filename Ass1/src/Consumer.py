5a1a9cdf61bbe7318fdbcb4e02fc9563b07a645a
GMoylan02 <gerardmoylan02@gmail.com>
2023-09-22 00:59:03 +0100

    made progress, refurbished producer

--- Ass1/src/Consumer.py
+++ Ass1/src/Consumer.py
@@ -1,13 +1,25 @@
 import socket
 bufferSize = 1024
 class Consumer:
-    def __init__(self, number):
-        self.name = f'Consumer{number}'
-        self.header = number.toBytes(1, 'big')
+    def __init__(self, id):
+        self.id = id
+        self.name = f'Consumer{id}'
         self.UDPsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.UDPsocket.bind((self.name, 50000))
 
-    # Where stream_id is the producer id + stream_no
-    def subscribe(self, stream_id):
-        self.header = self.header + stream_id
+    def subscribe(self, stream_no):
+        header = self.id + stream_no
+        subscribe = str.encode("subscribe")
+        self.UDPsocket.sendto(header + subscribe, ("Broker", 50000))
+
+    def listen(self):
+        while True:
+            incoming_msg = self.UDPsocket.recvfrom(bufferSize)
+            is_announcement = incoming_msg[5:5] == 0x00
+            stream_no = incoming_msg[0]
+            if is_announcement and stream_no % self.number == 0: # Just arbritrary, consumer m subs to producer n if m is a multiple of n
+                self.subscribe(incoming_msg)
+            else:
+                print("Received a frame!")
+
 
--- Ass1/src/Producer.py
+++ Ass1/src/Producer.py
@@ -1,34 +1,43 @@
 import socket
-import os
+import time
 localIP = "Producer"
 
 localPort = 50000
 bufferSize = 1024
 video_path = '\..\First20Frames'
 
+
 # TODO allow for 1 producer to have multiple streams
 class Producer:
-    def __init__(self, id: bytes, stream_no: bytes, number):
-        self.id = id
-        self.stream_no = stream_no
-        self.header = id + stream_no
-        self.number = number
+    def __init__(self, producer_id: bytes):
+        self.id = producer_id
+        self.no_streams = 0
         self.UDPsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.UDPsocket.bind((localIP, localPort))
 
     # TODO Include functionality for the producer to wait for subscriptions before continuing
-    def notify_broker(self):
+    def notify_broker(self, stream_no):
         msg = str.encode(f"Announcing the topic contained in this header!")
-        self.UDPsocket.sendto(self.header + 0x0000 + msg, ("Broker", 50000))
+        header = self.id + stream_no.to_bytes(1, 'big') + 0x0000.to_bytes(2, 'big')
+        self.UDPsocket.sendto(header + msg, ("Broker", 50000))
 
     # Publishes the videos as packets to the broker
     # Assumes the frames follow the file naming convention of "frame001.png" and so on
     # TODO implement functionality to send audio as well
-    def publish(self):
-        bytesToSend = None
+    def publish(self, stream_no):
+        bytes_to_send = bytearray()
         for i in range(1, 21):
-            bytesToSend.extend(png_to_bytearray(f"{video_path}\frame{i:03d}.png"))
-            self.UDPsocket.sendto(self.header + i.to_bytes(1, 'big') + 0x01 + bytesToSend[i-1], ("Broker", 50000))
+            bytes_to_send.extend(png_to_bytearray(f"{video_path}\frame{i:03d}.png"))
+            header = self.id + stream_no + i.to_bytes(1, 'big') + 0x01  # See txt file for byte break down
+            self.UDPsocket.sendto(header + bytes_to_send[i-1], ("Broker", 50000))
+
+# Increment the number of streams, notify the broker, give ample time for consumers to sub, and then publish frames
+    def new_stream(self):
+        self.no_streams += 1
+        self.notify_broker(self.no_streams)
+        time.sleep(1)
+        self.publish(self.no_streams)
+
 
 # Given the filepath, converts a png file to a bytearray
 def png_to_bytearray(filepath: str):
